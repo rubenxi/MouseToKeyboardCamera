526340370a1e55308fdbe0bcae570ac64e160e2f
RubenXI <rubenmolinalozano.rml@uma.es>
2019-05-28 00:17:41 +0200

    Added more options to customize usage
    -Now you can select if you want to use unclutter and make the mouse invisible, or don´t use it, or map clicks and make the mouse invisible, etc....Many combinations allowed so anyone can decide what to use.
    
    +Now you can use mouseclick even if you don´t have unclutter, but you need to pass an additional argument "y" to activate it or "n" to not.

--- mouseclick.c
+++ mouseclick.c
@@ -20,8 +20,8 @@ void exhandler(int ex) {
 int main(int argc, char *argv[])
 {
         signal(SIGINT, exhandler);
-        if (argc != 8 && argc != 3){
-        system("xdotool key I N C O R R E C T space A R G U M E N T S space P L E A S E space P A S S space 2 space O R space 7 space L E T T E R S space T O space M O U S E C L I C K key Return");
+        if (argc != 9 && argc != 4){
+        system("xdotool key I N C O R R E C T space A R G U M E N T S space P L E A S E space P A S S space 2 space O R space 7 space L E T T E R S space A N D space S E T space T H E space L A S T space A R G U M E N T space T O space y space T O space U S E space U N C L U T T E R space A N D space M A K E space T H E space M O U S E space D I S A P P E A R space O R space n space T O space N O T key Return");
         system ("killall mouseclick");
         kill(getpid(), 1);
         }
@@ -30,10 +30,29 @@ int main(int argc, char *argv[])
     
     sleep (3);
     system("xdotool key o sleep 1 key o sleep 1 key o sleep 1 key Return");
+
+
+    char * unc9=argv[8]; 
+    char * unc4=argv[3]; 
+    
+const char* str9 = unc9;  
+const char* str4 = unc4;
+
+int cmp9 = strcmp ("y", str9);
+int cmp4 = strcmp ("y", str4);
+
+
+
+    
+
+//UNCLUTTER ACTIVATED:
+
+if ( (argc == 9 && cmp9 == 0 ) || (argc == 4 && cmp4 == 0) )    
+{
     
     
       //Long version
-   if ( argc == 8 ) {
+   if ( argc == 9 ) {
 char * x=argv[1]; //left
 char xdoXdown[100];
 char xdoXup[100];
@@ -211,7 +230,7 @@ sprintf(xdoextra2up, "xdotool keyup %s", extra2);
         watch=0;
         
         
-    system("xdotool key C L I C K space A space K E Y space I N space M O U S E space F O R space E X T R A B U T T O N 1 space B U T T O N  ");
+    system("xdotool key C L I C K space A space K E Y space I N space M O U S E space F O R space E X T R A space B U T T O N space 1  ");
     
     sleep(1);
             while (watch == 0){
@@ -230,7 +249,7 @@ sprintf(xdoextra2up, "xdotool keyup %s", extra2);
                         }
                     }
         watch=0;
-    system("xdotool key C L I C K space A space K E Y space I N space M O U S E space F O R space E X T R A B U T T O N 2 space B U T T O N  ");
+    system("xdotool key C L I C K space A space K E Y space I N space M O U S E space F O R space E X T R A space B U T T O N space 2");
     
     sleep(1);
     
@@ -401,7 +420,7 @@ sprintf(xdoextra2up, "xdotool keyup %s", extra2);
    
    //Short version
    
-   else if (argc == 3) {
+   else if (argc == 4) {
        
 char * x=argv[1]; //left
 char xdoXdown[100];
@@ -543,6 +562,529 @@ sprintf(xdoAup, "xdotool keyup %s", a);
     
 
    }
+}
+
+
+
+
+
+//UNCLUTTER DESACTIVATED
+//-----------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+else if ( (argc == 9 && cmp9 != 0) || (argc == 4 && cmp4 != 0) )    
+{
+
+      //Long version
+   if ( argc == 9 ) {
+char * x=argv[1]; //left
+char xdoXdown[100];
+char xdoXup[100];
+sprintf(xdoXdown, "xdotool keydown %s", x);
+sprintf(xdoXup, "xdotool keyup %s", x);
+
+char * a=argv[2]; //right
+char xdoAdown[100];
+char xdoAup[100];
+sprintf(xdoAdown, "xdotool keydown %s", a);
+sprintf(xdoAup, "xdotool keyup %s", a);
+
+char * midb=argv[3]; //midb
+char xdomiddown[100];
+char xdomidup[100];
+sprintf(xdomiddown, "xdotool keydown %s", midb);
+sprintf(xdomidup, "xdotool keyup %s", midb);
+
+char * wheelup=argv[4]; //wheelup 
+char xdowheelupdown[100];
+char xdowheelupup[100];
+sprintf(xdowheelupdown, "xdotool keydown %s", wheelup);
+sprintf(xdowheelupup, "xdotool keyup %s", wheelup);
+
+char * wheeldown=argv[5]; //wheeldown
+char xdowheeldowndown[100];
+char xdowheeldownup[100];
+sprintf(xdowheeldowndown, "xdotool keydown %s", wheeldown);
+sprintf(xdowheeldownup, "xdotool keyup %s", wheeldown);
+
+char * extra1=argv[6]; //extra1
+char xdoextra1down[100];
+char xdoextra1up[100];
+sprintf(xdoextra1down, "xdotool keydown %s", extra1);
+sprintf(xdoextra1up, "xdotool keyup %s", extra1);
+
+char * extra2=argv[7]; //extra2
+char xdoextra2down[100];
+char xdoextra2up[100];
+sprintf(xdoextra2down, "xdotool keydown %s", extra2);
+sprintf(xdoextra2up, "xdotool keyup %s", extra2);
+
+
+
+    Display *display;
+    XEvent xevent;
+    Window window;
+
+    if( (display = XOpenDisplay(NULL)) == NULL )
+        return -1;
+
+
+    window = DefaultRootWindow(display);
+    XAllowEvents(display, AsyncBoth, CurrentTime);
+
+    XGrabPointer(display, 
+                 window,
+                 1, 
+                 PointerMotionMask | ButtonPressMask | ButtonReleaseMask , 
+                 GrabModeAsync,
+                 GrabModeAsync, 
+                 None,
+                 None,
+                 CurrentTime);
     
+
+    //keys assignment
+    
+
+    int izquierdaid;
+    int derechaid;
+    int centroid;
+    int ruedaarribaid;
+    int ruedaabajoid;
+    int botonlateral1id;
+    int botonlateral2id;
+    int watch = 0;
+    
+    sleep (3);
+    system("xdotool key C L I C K space A space K E Y space I N space M O U S E space F O R space L E F T space B U T T O N  ");
+    
+    sleep(1);
+    while (watch == 0){
+        XNextEvent(display, &xevent);
+     switch (xevent.type) {
+                     
+             
+       case ButtonPress:
+           izquierdaid = xevent.xbutton.button;
+           watch = 1;
+    system ("xdotool key Return");
+    system("xdotool key D O N E ");
+    system ("xdotool key Return");
+    sleep(1);
+           break;
+                        }
+                    }
+        watch=0;
+    system("xdotool key C L I C K space A space K E Y space I N space M O U S E space F O R space R I G H T space B U T T O N  ");
+    
+    sleep(1);
+    
+        while (watch == 0){
+                     XNextEvent(display, &xevent);
+     switch (xevent.type) {
+             
+       case ButtonPress:
+           derechaid = xevent.xbutton.button;
+           watch = 1;
+    system ("xdotool key Return");
+    system("xdotool key D O N E ");
+    system ("xdotool key Return");
+           break;
+                        }
+                    }
+     
+        watch=0;
+    system("xdotool key C L I C K space A space K E Y space I N space M O U S E space F O R space W H E E L space U P space B U T T O N  ");
+    
+    sleep(1);
+            while (watch == 0){
+                     XNextEvent(display, &xevent);
+     switch (xevent.type) {
+             
+       case ButtonPress:
+            
+           ruedaarribaid = xevent.xbutton.button;
+           watch = 1;
+    system ("xdotool key Return");
+    system("xdotool key D O N E ");
+    system ("xdotool key Return");
+            
+           break;
+                        }
+                    }
+        watch=0;
+    system("xdotool key C L I C K space A space K E Y space I N space M O U S E space F O R space W H E E L space D O W N space B U T T O N  ");
+    
+    sleep(1);
+            while (watch == 0){
+                     XNextEvent(display, &xevent);
+     switch (xevent.type) {
+             
+       case ButtonPress:
+           if (xevent.xbutton.button != ruedaarribaid){
+           ruedaabajoid = xevent.xbutton.button;
+           watch = 1;
+    system ("xdotool key Return");
+    system("xdotool key D O N E ");
+    system ("xdotool key Return");
+           }
+           break;
+                        }
+                    }
+                    
+    watch=0;
+    system("xdotool key C L I C K space A space K E Y space I N space M O U S E space F O R space M I D D L E space B U T T O N  ");
+    
+    sleep(1);
+            while (watch == 0){
+                     XNextEvent(display, &xevent);
+     switch (xevent.type) {
+             
+       case ButtonPress:
+           if ( xevent.xbutton.button != ruedaabajoid && xevent.xbutton.button != ruedaarribaid ){
+           centroid = xevent.xbutton.button;
+           watch = 1;
+    system ("xdotool key Return");
+    system("xdotool key D O N E ");
+    system ("xdotool key Return");
+           }
+           break;
+                        }
+                    }
+        watch=0;
+        
+        
+    system("xdotool key C L I C K space A space K E Y space I N space M O U S E space F O R space E X T R A space B U T T O N space 1 ");
+    
+    sleep(1);
+            while (watch == 0){
+                     XNextEvent(display, &xevent);
+     switch (xevent.type) {
+             
+       case ButtonPress:
+            if ( xevent.xbutton.button != ruedaabajoid && xevent.xbutton.button != ruedaarribaid ){
+           botonlateral1id = xevent.xbutton.button;
+           watch = 1;
+    system ("xdotool key Return");
+    system("xdotool key D O N E ");
+    system ("xdotool key Return");
+            }
+           break;
+                        }
+                    }
+        watch=0;
+    system("xdotool key C L I C K space A space K E Y space I N space M O U S E space F O R space E X T R A space B U T T O N space 2  ");
+    
+    sleep(1);
+    
+        while (watch == 0){
+                     XNextEvent(display, &xevent);
+     switch (xevent.type) {
+             
+       case ButtonPress:
+              if ( xevent.xbutton.button != ruedaabajoid && xevent.xbutton.button != ruedaarribaid ){
+           botonlateral2id = xevent.xbutton.button;
+           watch = 1;
+    system ("xdotool key Return");
+    system("xdotool key D O N E ");
+    system ("xdotool key Return");
+              }
+           break;
+                        }
+                    }
+        watch=0;
+    system("xdotool key C O N F I G U R A T I O N space F I N I S H E D key Return ");
+    system("xdotool key C H A N G E space T O space G A M E space T A B space N O W key Return ");
+    system("xdotool key D O space A space C T R L space C space I N space T H I S space W I N D O W space T O space E X I T key Return");
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+       while( 1 == 1 ) {
+    
+     XNextEvent(display, &xevent);
+    
+    
+     switch (xevent.type) {
+             
+           
+            case ButtonPress:
+                
+                if (xevent.xbutton.button == izquierdaid) {
+                    
+                         system(xdoXdown);
+
+                }
+                    
+                    
+               
+                   else if (xevent.xbutton.button ==  derechaid){
+                         system(xdoAdown);
+
+                    }
+                    
+                    
+               
+                  else  if (xevent.xbutton.button ==  centroid){
+                         system(xdomiddown);
+
+       }
+                    
+                    
+                
+                   else if (xevent.xbutton.button ==  ruedaarribaid){
+                         system(xdowheelupdown);
+
+                        }
+                    
+                    
+           
+                  else  if (xevent.xbutton.button ==  ruedaabajoid){
+                         system(xdowheeldowndown);
+
+                        }
+                    
+                    
+              
+                  else  if (xevent.xbutton.button ==  botonlateral1id){
+                         system(xdoextra1down);
+
+                    }
+                    
+            
+                   else if (xevent.xbutton.button ==  botonlateral2id){
+                         system(xdoextra2down);
+
+                    }
+                    
+                    
+                
+                    
+               break; //break buttonpress
+               
+               
+                    case ButtonRelease:
+                        
+                          
+                   if (xevent.xbutton.button ==  izquierdaid){
+                         system(xdoXup);
+
+                    }
+                    
+                    
+               
+                   else if (xevent.xbutton.button ==  derechaid){
+                         system(xdoAup);
+
+                    }
+                    
+                    
+                else    if (xevent.xbutton.button ==  centroid){
+                         system(xdomidup);
+
+                    }
+                    
+                    
+                  else  if (xevent.xbutton.button ==  ruedaarribaid){
+                         system(xdowheelupup);
+
+                    }
+                    
+                    
+                  else  if (xevent.xbutton.button ==  ruedaabajoid){
+                         system(xdowheeldownup);
+
+                    }
+                    
+                    
+                   else if (xevent.xbutton.button ==  botonlateral1id){
+                         system(xdoextra1up);
+
+                    }
+                    
+                    
+                   else if (xevent.xbutton.button ==  botonlateral2id){
+                         system(xdoextra2up);
+
+                    }
+                    
+                    
+                
+                
+                break; //break buttonrelease
+               
+     }
+     
+       } //while end
+    
+    
+   }
+   
+   //Short version
+   
+   else if (argc == 4) {
+       
+char * x=argv[1]; //left
+char xdoXdown[100];
+char xdoXup[100];
+sprintf(xdoXdown, "xdotool keydown %s", x);
+sprintf(xdoXup, "xdotool keyup %s", x);
+
+char * a=argv[2]; //right
+char xdoAdown[100];
+char xdoAup[100];
+sprintf(xdoAdown, "xdotool keydown %s", a);
+sprintf(xdoAup, "xdotool keyup %s", a);
+
+    Display *display;
+    XEvent xevent;
+    Window window;
+
+    if( (display = XOpenDisplay(NULL)) == NULL )
+        return -1;
+
+
+    window = DefaultRootWindow(display);
+    XAllowEvents(display, AsyncBoth, CurrentTime);
+
+    XGrabPointer(display, 
+                 window,
+                 1, 
+                 PointerMotionMask | ButtonPressMask | ButtonReleaseMask , 
+                 GrabModeAsync,
+                 GrabModeAsync, 
+                 None,
+                 None,
+                 CurrentTime);
+    
+    //keys assignment
+    
+
+    int izquierdaid;
+    int derechaid;
+
+    int watch = 0;
+    
+    
+    system("xdotool key C L I C K space A space K E Y space I N space M O U S E space F O R space L E F T space B U T T O N  ");
+    
+    sleep(1);
+    while (watch == 0){
+        XNextEvent(display, &xevent);
+     switch (xevent.type) {
+                     
+             
+       case ButtonPress:
+           izquierdaid = xevent.xbutton.button;
+           watch = 1;
+    system ("xdotool key Return");
+    system("xdotool key D O N E ");
+    system ("xdotool key Return");
+    sleep(1);
+           break;
+                        }
+                    }
+        watch=0;
+    system("xdotool key C L I C K space A space K E Y space I N space M O U S E space F O R space R I G H T space B U T T O N  ");
+    
+    sleep(1);
+    
+        while (watch == 0){
+                     XNextEvent(display, &xevent);
+     switch (xevent.type) {
+             
+       case ButtonPress:
+           derechaid = xevent.xbutton.button;
+           watch = 1;
+    system ("xdotool key Return");
+    system("xdotool key D O N E ");
+    system ("xdotool key Return");
+           break;
+                        }
+                    }
+        watch=0;  
+        system("xdotool key C O N F I G U R A T I O N space F I N I S H E D key Return ");
+    system("xdotool key C H A N G E space T O space G A M E space T A B space N O W key Return ");
+    system("xdotool key D O space A space C T R L space C space I N space T H I S space W I N D O W space T O space E X I T key Return");
+    
+    
+       while( 1 == 1 ) {
+    
+     XNextEvent(display, &xevent);
+    
+    
+     switch (xevent.type) {
+             
+           
+            case ButtonPress:
+                
+                if (xevent.xbutton.button == izquierdaid) {
+                    
+                         system(xdoXdown);
+
+                }
+                    
+                    
+               
+                   else if (xevent.xbutton.button ==  derechaid){
+                         system(xdoAdown);
+
+                    }
+                    
+
+                    
+               break; //break buttonpress
+               
+               
+                    case ButtonRelease:
+                        
+                          
+                   if (xevent.xbutton.button ==  izquierdaid){
+                         system(xdoXup);
+
+                    }
+                    
+                    
+               
+                   else if (xevent.xbutton.button ==  derechaid){
+                         system(xdoAup);
+
+                    }
+                    
+ 
+                break; //break buttonrelease
+               
+     }
+     
+       } //while end
+    
+
+   }
+    
+}
     return 0;
 }
